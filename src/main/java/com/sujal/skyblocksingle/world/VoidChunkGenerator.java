0ff38612ccc4b5762db73c3bed644ebc71aeba9a
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-01 11:47:24 +0530

    Create VoidChunkGenerator.java

---
+++ src/main/java/com/sujal/skyblocksingle/world/VoidChunkGenerator.java
@@ -0,0 +1,83 @@
+package com.sujal.skyblocksingle.world;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.block.BlockState;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.HeightLimitView;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.biome.source.BiomeSource;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.Blender;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.VerticalBlockSample;
+import net.minecraft.world.gen.noise.NoiseConfig;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class VoidChunkGenerator extends ChunkGenerator {
+    public static final Codec<VoidChunkGenerator> CODEC = RecordCodecBuilder.create((instance) ->
+            instance.group(BiomeSource.CODEC.fieldOf("biome_source")
+                    .forGetter((generator) -> generator.biomeSource)
+            ).apply(instance, instance.stable(VoidChunkGenerator::new)));
+
+    public VoidChunkGenerator(BiomeSource biomeSource) {
+        super(biomeSource);
+    }
+
+    @Override
+    protected Codec<? extends ChunkGenerator> getCodec() {
+        return CODEC;
+    }
+
+    @Override
+    public void carve(ChunkRegion region, long seed, NoiseConfig noiseConfig, BiomeAccess world, StructureAccessor structureAccessor, Chunk chunk, GenerationStep.Carver carverStep) {
+    }
+
+    @Override
+    public void buildSurface(ChunkRegion region, StructureAccessor structures, NoiseConfig noiseConfig, Chunk chunk) {
+    }
+
+    @Override
+    public void populateEntities(ChunkRegion region) {
+    }
+
+    @Override
+    public int getWorldHeight() {
+        return 384;
+    }
+
+    @Override
+    public CompletableFuture<Chunk> populateNoise(Executor executor, Blender blender, NoiseConfig noiseConfig, StructureAccessor structureAccessor, Chunk chunk) {
+        return CompletableFuture.completedFuture(chunk);
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getMinimumY() {
+        return -64;
+    }
+
+    @Override
+    public int getHeight(int x, int z, Heightmap.Type heightmap, HeightLimitView world, NoiseConfig noiseConfig) {
+        return 0;
+    }
+
+    @Override
+    public VerticalBlockSample getColumnSample(int x, int z, HeightLimitView world, NoiseConfig noiseConfig) {
+        return new VerticalBlockSample(0, new BlockState[0]);
+    }
+
+    @Override
+    public void getDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {
+    }
+}
