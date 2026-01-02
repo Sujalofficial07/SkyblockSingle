12159f7d816d379f442242a56eb108acc3ad004f
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 08:43:09 +0530

    Create SBConstants.java

---
+++ skyblock-api/src/main/java/com/sb/api/utils/SBConstants.java
@@ -0,0 +1,7 @@
+package com.sb.api.utils;
+
+public class SBConstants {
+    public static final String NBT_ROOT = "SkyBlockData";
+    public static final String NBT_STATS = "Stats";
+    public static final String NBT_PROFILE = "Profile";
+}
