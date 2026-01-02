103ccebe21e1ac4d47b53958197460fb03dcb1ca
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 09:41:13 +0530

    Update SkyBlockItems.java

--- skyblock-items/src/main/java/com/sb/items/SkyBlockItems.java
+++ skyblock-items/src/main/java/com/sb/items/SkyBlockItems.java
@@ -1,18 +1,22 @@
 package com.sb.items;
 
+import com.sb.items.commands.TestItemCommand; // Import the command
 import com.sb.items.events.TooltipHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback; // Fabric API for commands
 
 public class SkyBlockItems implements ModInitializer, ClientModInitializer {
     @Override
     public void onInitialize() {
-        // Server side logic (Items register karna)
+        // ✅ Register Command Here
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            TestItemCommand.register(dispatcher);
+        });
     }
 
     @Override
     public void onInitializeClient() {
-        // Tooltip sirf client side par hota hai
         TooltipHandler.register();
     }
 }
