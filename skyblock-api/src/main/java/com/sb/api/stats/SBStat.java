2995a125e9e92cf83f56241c280cf6b0cb5110b7
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 09:11:40 +0530

    Create SBStat.java

---
+++ skyblock-api/src/main/java/com/sb/api/stats/SBStat.java
@@ -0,0 +1,12 @@
+package com.sb.api.stats;
+
+public enum SBStat {
+    HEALTH,
+    DEFENSE,
+    STRENGTH,
+    SPEED,
+    CRIT_CHANCE,
+    CRIT_DAMAGE,
+    INTELLIGENCE, // Mana ke liye
+    MAGIC_FIND;
+}
