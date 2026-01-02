457b19958b1193d79f8393bb5e8c1253cfb5b6a3
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 09:12:24 +0530

    Update IPlayerStats.java

--- skyblock-api/src/main/java/com/sb/api/stats/IPlayerStats.java
+++ skyblock-api/src/main/java/com/sb/api/stats/IPlayerStats.java
@@ -1,19 +1,14 @@
-// Path: skyblock-api/src/main/java/com/sb/api/stats/IPlayerStats.java
 package com.sb.api.stats;
 
 import dev.onyxstudios.cca.api.v3.component.ComponentV3;
+import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent;
 
-public interface IPlayerStats extends ComponentV3 {
-    double getStrength();
-    void setStrength(double value);
+// ServerTickingComponent add kiya taaki Mana regen ho sake
+public interface IPlayerStats extends ComponentV3, ServerTickingComponent {
+    double getBaseStat(SBStat stat);
+    void setBaseStat(SBStat stat, double value);
     
-    double getCritChance();
-    double getCritDamage();
-    
-    double getIntelligence();
-    void setIntelligence(double value);
-    
-    // Mana logic
     double getCurrentMana();
     void consumeMana(double amount);
+    void regenMana();
 }
