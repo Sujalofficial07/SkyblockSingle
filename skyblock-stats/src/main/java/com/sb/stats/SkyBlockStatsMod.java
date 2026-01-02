e9575da3d3cbb9e3dbb0b2363d93343f02a3bb1f
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-02 13:22:43 +0530

    Update SkyBlockStatsMod.java

--- skyblock-stats/src/main/java/com/sb/stats/SkyBlockStatsMod.java
+++ skyblock-stats/src/main/java/com/sb/stats/SkyBlockStatsMod.java
@@ -1,37 +1,13 @@
 package com.sb.stats;
 
-import com.sb.stats.component.PlayerStatsComponent;
-import dev.onyxstudios.cca.api.v3.component.ComponentKey;
-import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
-import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
-import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
-import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.Identifier;
 
-// ✅ Ek hi class mein ModInitializer aur EntityComponentInitializer dono
-public class SkyBlockStatsMod implements ModInitializer, EntityComponentInitializer {
-
-    // 1. Component Key Register (Yehi ChatGPT ne bataya tha)
-    public static final ComponentKey<PlayerStatsComponent> PLAYER_STATS =
-            ComponentRegistry.getOrCreate(
-                    new Identifier("skyblock-stats", "player_stats"),
-                    PlayerStatsComponent.class
-            );
+public class SkyBlockStatsMod implements ModInitializer {
+    // Ab yahan koi "PLAYER_STATS" ya "EntityComponentInitializer" nahi hai.
+    // Ye file sirf mod ko start karegi.
 
     @Override
     public void onInitialize() {
-        // Console log taaki pata chale mod load hua
         System.out.println("SkyBlock Stats Mod Initialized!");
-    }
-
-    @Override
-    public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
-        // 2. Player ke liye Stats attach karna
-        registry.registerForPlayers(
-                PLAYER_STATS,
-                PlayerStatsComponent::new,
-                RespawnCopyStrategy.ALWAYS_COPY // Marne par stats save rahenge
-        );
     }
 }
